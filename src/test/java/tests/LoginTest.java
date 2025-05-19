b7977e7fa94e2ef76b1cbf42b80acda40371cdd4
harinath72 <harinath72@gmail.com>
2025-05-19 21:39:39 +0530

    DDT

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,26 +1,55 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtils;
 import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest{
 	
 	
-	@Test
-	public void testValidLogin() {
+	@DataProvider(name="LoginData")
+	public Object[][] getLoginData() throws IOException{
+		String filePath = System.getProperty("user.dir")+"/testdata/TestData.xlsx";
+		ExcelUtils.loadExcel(filePath, "Sheet1");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount-1][2];
+		for(int i=1;i<rowCount;i++) {
+			data[i-1][0] = ExcelUtils.getCellData(i, 0); //userName
+			data[i-1][1] = ExcelUtils.getCellData(i, 1); //password
+		}
+		ExcelUtils.closeExcel();
+		return data;
+	}
+	
+	@DataProvider(name = "LoginData2")
+	public Object[][] getData(){
+		return new Object[][] {
+			{"user1","pass1"},
+			{"user2","pass2"},
+			{"user3","pass3"}
+		};
+	}
+	
+	@Test(dataProvider = "LoginData2")
+	public void testValidLogin(String username,String password) {
 		
 		Log.info("Starting login test...");
-		test = ExtentReportManager.createTest("Login Test");
+		test = ExtentReportManager.createTest("Login Test: "+username);
 		test.info("Navegating to URL");
 		
 		LoginPage loginPage = new LoginPage(driver);
-		loginPage.enterUsername("admin@yourstore.com");
-		loginPage.enterPassword("admin");
+		//loginPage.enterUsername("admin@yourstore.com");
+		//loginPage.enterPassword("admin");
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
 		
 		test.info("Clicking on Login button");
 		loginPage.clickLogin();
@@ -33,7 +62,7 @@ public class LoginTest extends BaseTest{
 		
 	}
 	
-	@Test
+/*	@Test
    public void testLoginWithInvalidCredentials() {
 		
 		Log.info("Starting login test...");
@@ -54,5 +83,6 @@ public class LoginTest extends BaseTest{
 		test.pass("Login successful");
 		
 	}
+	*/
 
 }
